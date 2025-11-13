4f8e8b7e1d212a12d76da378eaf2927183955ce2
alina <a72658695@gmail.com>
2025-11-13 20:27:50 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Laba14_Vah_Kuz
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
